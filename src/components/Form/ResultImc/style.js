c5ab9515612f90dcfa838943b919e2320de083c5
larissaralmeida <larissaralmeida.94@gmail.com>
2022-07-19 21:54:25 -0300

    :sparkles: Create simple IMC app in React Native

---
+++ src/components/Form/ResultImc/style.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    resultImc: {
+        flex:0.7,
+        marginTop:15,
+        paddingTop:60,
+        borderRadius:50,
+        alignItems:'center',
+        width:'100%',
+        fontSize:48,
+        color:'#5F9EA0',
+        fontWeight: 'bold',
+        paddingBottom:60,
+        width:'90%',
+        height:'90%'
+
+    },
+
+    numberImc:{
+        fontSize:48,
+        color:'#5F9EA0',
+        fontWeight: 'bold'
+
+    },
+
+    information:{
+        fontSize:18,
+        color:'#5F9EA0',
+        fontWeight: 'bold'
+    }
+
+});
+
+export default styles;
---
+++ src/components/Title/style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    boxTitle: {
+        alignItems:'center',
+        justifyContent:'center',
+        padding: 10,
+    },
+    textTitle: {
+        color:'#5F9EA0',
+        fontSize:24,
+        fontWeight:'bold',
+    }
+
+
+});
+
+
+export default styles;
